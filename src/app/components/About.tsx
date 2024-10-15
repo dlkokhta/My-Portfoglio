575041180e1e58b38b609d72692f14eb20837df6
unknown <dl.kokhtashvili@gmail.com>
2024-10-15 21:14:31 +0400

    change about me text

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -5,16 +5,13 @@ const About = () => {
         <h1 className="text-xl mb-5 font-bold">About Me</h1>
 
         <p>
-          Hello! I&apos;m Dmitri Kokhtashvili, a passionate full-stack developer
-          with a strong interest in building modern, responsive, and efficient
-          web applications. I specialize in using technologies like React,
-          Next.js, and Express to create user-friendly interfaces and scalable
-          backend systems. I enjoy taking on new challenges and continuously
-          learning to enhance my skills in full-stack development. In addition
-          to coding, I&apos;m dedicated to improving user experience and
-          performance, ensuring every project I work on is both functional and
-          engaging. Feel free to explore my portfolio and check out my latest
-          projects!
+          Hello! I&apos;m Dimitri Kokhtashvili, a front-end-focused full-stack
+          developer specializing in React and Next.js. I have a passion for
+          building dynamic, responsive user interfaces and have experience
+          creating efficient and scalable back-end systems with Express.js. My
+          primary focus is on front-end development, ensuring a seamless user
+          experience, but I also bring full-stack knowledge to projects that
+          require it.
         </p>
       </div>
       <div className="to-transparen h-[0.5px] bg-gradient-to-r from-transparent via-slate-400  mt-10"></div>
