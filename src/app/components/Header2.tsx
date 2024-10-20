444ab681e6383ecbadaf644dd07cfdb01e4f1555
unknown <dl.kokhtashvili@gmail.com>
2024-10-20 23:48:10 +0400

    Enhance menu functionality to change text color based on active section during scrolling

--- src/app/components/Header2.tsx
+++ src/app/components/Header2.tsx
@@ -8,7 +8,7 @@ const Header2 = () => {
         <h1 className="text-4xl font-extrabold mb-3">Dimitri Kokhtashvili</h1>
         <h2 className="text-center">Full Stack Developer</h2>
       </div>
-      <div className="fixed flex flex-col mt-[150px] xl:mt-[220px] lg:gap-3">
+      <div className="fixed flex flex-col mt-[150px] xl:mt-[220px] lg:gap-5">
         <Menu />
       </div>
       <div className="fixed flex flex-col gap-10 mt-[370px] text-[14px] xl:mt-[480px]">
--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,33 +1,52 @@
 import { Link } from "react-scroll";
-const Menu = ({}) => {
+
+const Menu = () => {
   return (
     <>
-      <Link to="about" smooth={true} duration={500} className="cursor-pointer ">
-        <div className="w-full flex items-center ">
-          <h1 className="">About</h1>
+      <Link
+        to="about"
+        smooth={true}
+        duration={500}
+        spy={true}
+        activeClass="active"
+        className="cursor-pointer"
+      >
+        <div className="w-full flex items-center">
+          <h1 className="">ABOUT</h1>
         </div>
       </Link>
 
-      <Link to="skills" smooth={true} duration={500} className="cursor-pointer">
-        <h1>Skills</h1>
+      <Link
+        to="skills"
+        smooth={true}
+        duration={500}
+        spy={true}
+        activeClass="active"
+        className="cursor-pointer"
+      >
+        <h1>SKILLS</h1>
       </Link>
 
       <Link
         to="projects"
         smooth={true}
         duration={500}
+        spy={true}
+        activeClass="active"
         className="cursor-pointer "
       >
-        <h1>Projects</h1>
+        <h1>PROJECTS</h1>
       </Link>
 
       <Link
         to="contact"
         smooth={true}
         duration={500}
+        spy={true}
+        activeClass="contact"
         className="cursor-pointer lg:hidden"
       >
-        <h1>Contact Me</h1>
+        <h1>CONTACT</h1>
       </Link>
 
       <a
