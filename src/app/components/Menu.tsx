13452c194819256fdef32c079b5ae06e33f1f81c
unknown <dl.kokhtashvili@gmail.com>
2025-05-04 23:14:38 +0400

    update resume

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -4,11 +4,11 @@ import { use, useState } from "react";
 const Menu = () => {
   const [activeLink, setActiveLink] = useState("about");
 
-  const handleSetActive = (to:any) => {
+  const handleSetActive = (to: any) => {
     setActiveLink(to);
   };
 
-  const handleClick = (to:any) => {
+  const handleClick = (to: any) => {
     setActiveLink(to); // Immediately set the active link on click
   };
   return (
@@ -24,14 +24,15 @@ const Menu = () => {
         className={`cursor-pointer ${
           activeLink === "about" ? "text-gray-400" : ""
         }`}
-
       >
-         <div className="flex items-center gap-5 group">
-         <div
-      className={`h-0.5 w-10 transition-all duration-500 hidden lg:block ${
-        activeLink === "about"  ? "bg-gray-400 w-24" : "bg-white group-hover:bg-gray-400 group-hover:w-24"
-      }`}
-    ></div>
+        <div className="flex items-center gap-5 group">
+          <div
+            className={`h-0.5 w-10 transition-all duration-500 hidden lg:block ${
+              activeLink === "about"
+                ? "bg-gray-400 w-24"
+                : "bg-white group-hover:bg-gray-400 group-hover:w-24"
+            }`}
+          ></div>
           <h1 className="text-sm md:text-base">ABOUT</h1>
         </div>
       </Link>
@@ -46,15 +47,16 @@ const Menu = () => {
         className={`cursor-pointer transition-colors duration-700 ${
           activeLink === "skills" ? "text-gray-400" : ""
         }`}
-
       >
-         <div className="flex items-center gap-5 group">
-         <div
-      className={`h-0.5 w-10 transition-all duration-500 hidden lg:block ${
-        activeLink === "skills" ? "bg-gray-400 w-24" : "bg-white group-hover:bg-gray-400 group-hover:w-24"
-      }`}
-    ></div>
-        <h1 className="text-sm lg:text-base">SKILLS</h1>
+        <div className="flex items-center gap-5 group">
+          <div
+            className={`h-0.5 w-10 transition-all duration-500 hidden lg:block ${
+              activeLink === "skills"
+                ? "bg-gray-400 w-24"
+                : "bg-white group-hover:bg-gray-400 group-hover:w-24"
+            }`}
+          ></div>
+          <h1 className="text-sm lg:text-base">SKILLS</h1>
         </div>
       </Link>
 
@@ -70,15 +72,16 @@ const Menu = () => {
           activeLink === "projects" ? "text-gray-400" : ""
         }`}
       >
-         <div className="flex items-center gap-5 group">
-         <div
-      className={`h-0.5 w-10 transition-all duration-500 hidden lg:block ${
-        activeLink === "projects" ? "bg-gray-400 w-24" : "bg-white group-hover:bg-gray-400 group-hover:w-24"
-      }`}
-    ></div>
+        <div className="flex items-center gap-5 group">
+          <div
+            className={`h-0.5 w-10 transition-all duration-500 hidden lg:block ${
+              activeLink === "projects"
+                ? "bg-gray-400 w-24"
+                : "bg-white group-hover:bg-gray-400 group-hover:w-24"
+            }`}
+          ></div>
 
-        <h1 className="text-sm md:text-base">PROJECTS</h1>
-
+          <h1 className="text-sm md:text-base">PROJECTS</h1>
         </div>
       </Link>
 
@@ -95,16 +98,16 @@ const Menu = () => {
         }`}
       >
         <div className="flex items-center gap-5 group">
-        <div
-      className={`h-0.5 w-10 transition-all duration-500 hidden lg:block ${
-        activeLink === "certificates"  ? "bg-gray-400 w-24" : "bg-white group-hover:bg-gray-400 group-hover:w-24"
-      }`}
-    ></div>
-        <h1 className="text-sm md:text-base">CERTIFICATES</h1>
-
+          <div
+            className={`h-0.5 w-10 transition-all duration-500 hidden lg:block ${
+              activeLink === "certificates"
+                ? "bg-gray-400 w-24"
+                : "bg-white group-hover:bg-gray-400 group-hover:w-24"
+            }`}
+          ></div>
+          <h1 className="text-sm md:text-base">CERTIFICATES</h1>
         </div>
       </Link>
-
 
       <Link
         to="contactForm"
@@ -119,17 +122,16 @@ const Menu = () => {
         }`}
       >
         <div className="flex items-center gap-5 group">
-        <div
-      className={`h-0.5 w-10 transition-all duration-500 hidden lg:block ${
-        activeLink === "contactForm"  ? "bg-gray-400 w-24" : "bg-white group-hover:bg-gray-400 group-hover:w-24"
-      }`}
-    ></div>
-        <h1 className="text-sm md:text-base">CONTACT ME</h1>
-
+          <div
+            className={`h-0.5 w-10 transition-all duration-500 hidden lg:block ${
+              activeLink === "contactForm"
+                ? "bg-gray-400 w-24"
+                : "bg-white group-hover:bg-gray-400 group-hover:w-24"
+            }`}
+          ></div>
+          <h1 className="text-sm md:text-base">CONTACT ME</h1>
         </div>
-
       </Link>
-
 
       <a
         href="Dimitri Kokhtashvili.pdf"
